0c0405d591109cc3e940b3fe3e8c08a22b67e8ae
AndroidWithRossyn <118904953+AndroidWithRossyn@users.noreply.github.com>
2024-05-14 21:57:08 +0530

    14-05-2024

--- app/src/main/java/com/Udaicoders/wawbstatussaver/SplashActivity.java
+++ app/src/main/java/com/Udaicoders/wawbstatussaver/SplashActivity.java
@@ -3,22 +3,19 @@ package com.Udaicoders.wawbstatussaver;
 import static com.Udaicoders.wawbstatussaver.util.Utils.permissions;
 import static com.Udaicoders.wawbstatussaver.util.Utils.permissions13;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.Udaicoders.wawbstatussaver.intro.IntroActivity;
 import com.Udaicoders.wawbstatussaver.util.SharedPrefs;
 import com.Udaicoders.wawbstatussaver.util.Utils;
 
 public class SplashActivity extends AppCompatActivity {
-
-
-//    private int REQUEST_CODE_NOTIFICATION_LISTENER = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +26,24 @@ public class SplashActivity extends AppCompatActivity {
 
         Utils.setLanguage(SplashActivity.this, SharedPrefs.getLang(SplashActivity.this));
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
-        {
-            if (!Utils.hasPermissions(this, permissions13) && Utils.isNotificationServiceRunning(this)){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (!Utils.hasPermissions(this, permissions13) && Utils.isNotificationServiceRunning(this)) {
                 gotoNext();
-            }else {
+            } else {
+                gotoIntro();
+            }
+        } else {
+            if (!Utils.hasPermissions(this, permissions) && Utils.isNotificationServiceRunning(this)) {
+                gotoNext();
+            } else {
                 gotoIntro();
             }
         }
-        else
-        {
-            if (!Utils.hasPermissions(this, permissions) && Utils.isNotificationServiceRunning(this)){
-                gotoNext();
-            }else {
-                gotoIntro();
-            }
-        }
-
-
 
 
     }
 
-    void gotoNext(){
+    void gotoNext() {
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             finish();
@@ -59,16 +51,12 @@ public class SplashActivity extends AppCompatActivity {
 
     }
 
-    void gotoIntro(){
+    void gotoIntro() {
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, IntroActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             finish();
         }, 600);
 
     }
-
-    @Override
-    public void onBackPressed() { }
-
 
 }
